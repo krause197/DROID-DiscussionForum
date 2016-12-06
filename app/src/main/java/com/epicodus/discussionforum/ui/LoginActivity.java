91ebb724945cc10783794bcaef638c5c604cf9ff
Eric Krause and Stephen Emery <stephen@>
2016-12-06 09:47:07 -0800

    Add intent to move from login to createAccount activity.

--- app/src/main/java/com/epicodus/discussionforum/ui/LoginActivity.java
+++ app/src/main/java/com/epicodus/discussionforum/ui/LoginActivity.java
@@ -1,15 +1,34 @@
 package com.epicodus.discussionforum.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.epicodus.discussionforum.R;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.createTextView) TextView mCreateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+        mCreateTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mCreateTextView) {
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
--- app/src/main/java/com/epicodus/discussionforum/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/discussionforum/ui/MainActivity.java
@@ -3,6 +3,8 @@ package com.epicodus.discussionforum.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.epicodus.discussionforum.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
